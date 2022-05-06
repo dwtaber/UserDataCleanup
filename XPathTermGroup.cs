ae8f1a283bb5fbf3276131b4ec682ac9b254df90
dwtaber <dwtaber@gmail.com>
2022-05-05 22:05:08 -0400

    Hopefully fixed XPathTermGroup behavior

--- XPathTermGroup.cs
+++ XPathTermGroup.cs
@@ -2,21 +2,27 @@
 
 public class XPathTermGroup
 {
-    public List<XPathDataTerm>? XPathTerms { get; set; }
-    public List<XPathTermGroup>? XPathTermGroups { get; set; }
+    public List<XPathDataTerm> XPathTerms { get; set; } = new List<XPathDataTerm>();
+    public List<XPathTermGroup> XPathTermGroups { get; set; } = new List<XPathTermGroup>();
     public XPathBooleanOperator XPathBooleanOperator { get; set; }
     internal string separator => $" {XPathBooleanOperator} ";
-    public string? XPathTermsString => XPathTerms != null ? $"{ string.Join(separator, XPathTerms.Select(x => x.ToString()))}" : null;
-    public string? XPathTermGroupsString => XPathTermGroups != null ? $"{ string.Join(separator, XPathTermGroups.Select(x => x.ToString()))}" : null;
+    public string? XPathTermsString
+    {
+        get => XPathTerms.Count != 0 ? $"{ string.Join(separator, XPathTerms.Select(x => x.ToString()))}" : null;
+    }
+    public string? XPathTermGroupsString
+    {
+        get => XPathTermGroups.Count != 0 ? $"{ string.Join(separator, XPathTermGroups.Select(x => x.ToString()))}" : null;
+    }
     public override string ToString()
     {        
-        var hasLooseTerms = string.IsNullOrEmpty(XPathTermsString);
-        var hasGroups = string.IsNullOrEmpty(XPathTermGroupsString);
+        var hasLooseTerms = XPathTerms.Count != 0;
+        var hasGroups = XPathTermGroups.Count != 0;
 
         return (hasLooseTerms, hasGroups) switch
         {
-            (true, false) => XPathTermsString!,
-            (false, true) => XPathTermGroupsString!,
+            (true, false) => $"({XPathTermsString!})",
+            (false, true) => $"({XPathTermGroupsString!})",
             (true, true) => $"({string.Join(separator, XPathTermsString, XPathTermGroupsString)})",
             (false, false) => ""
         };
