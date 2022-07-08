cf3383c2564cb983c178b16f1c2208deba7083f0
Dan Taber <dwtaber@gmail.com>
2022-07-08 08:59:59 -0400

    Formatting fix

--- EventLogRecordExtension.cs
+++ EventLogRecordExtension.cs
@@ -4,7 +4,7 @@ public static class EventLogRecordExtension
 {
     public static object GetPropertyValue(this EventLogRecord record, string propertyName)
     {
-        var xPath = new List<string> {$"Event/EventData/Data[@Name='{propertyName}']"};
+        var xPath = new List<string> { $"Event/EventData/Data[@Name='{propertyName}']" };
         var selector = new EventLogPropertySelector(xPath);
         return record.GetPropertyValues(selector)[0];
     }
